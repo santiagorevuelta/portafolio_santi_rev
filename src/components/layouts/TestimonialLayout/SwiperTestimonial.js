f5f9d490a93630ddc7e45050600b6db5ec522665
Santi.rev <santiagorevuel@gmail.com>
2023-03-16 09:15:40 -0500

    mw

--- src/components/layouts/TestimonialLayout/SwiperTestimonial.js
+++ src/components/layouts/TestimonialLayout/SwiperTestimonial.js
@@ -3,11 +3,11 @@ import react, {useEffect, useState} from 'react'
 import Swiper from 'swiper'
 export default function SwiperTestimonial(){
     useEffect( () => {
-        let swiperTestimonial = new Swiper(".testimonial__container",{
+       new Swiper(".testimonial__container",{
             spaceBetween: 24,
             loop: true,
             grabCursor: true,
-        
+
             pagination: {
                 el: ".swiper-pagination",
                 clickable: true,
